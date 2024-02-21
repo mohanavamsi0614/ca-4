a3b5904e4b1d6a061e78b145e8b73fc081ff74e6
Mohana Vamsi <mohanavamsi14@gmail.com>
2024-02-21 10:18:13 +0530

    Add files via upload

--- src/Comp/Quesion.jsx
+++ src/Comp/Quesion.jsx
@@ -3,26 +3,25 @@ import question from "../quesions";
 import "./App.css"
 import { Link } from "react-router-dom";
 function Que() {
-    const [result,setresult]=useState({Attempted:0,correct:0,wrong:0})
+    const [result,setresult]=useState(0)
     const [curr,setcurr]=useState(0)
     const [high,sethigh]=useState(false)
     const [toggle,settoggle]=useState(true)
     const ref=useRef()
     const questions=question
     function add(i){
-        console.log(i,result.correct)
         if(curr<questions.length-1){
-        setcurr(curr+1)
-         if(i.isCorrect){
-            setresult({...result,Attempted:result.Attempted+1,correct:result.correct+1})
-        }
-        else{
-            setresult({...result,Attempted:result.Attempted+1,wrong:result.wrong+1})
+            setcurr(curr+1)
+
+        if(i.isCorrect){
+            setresult((pre)=>pre+1)
         }
         }
         else{
             ref.current.showModal()
         }
+        console.log(i,result)
+
     }
     function togle(){
          settoggle(!toggle)
@@ -54,20 +53,8 @@ function Que() {
                 <dialog ref={ref}>
                 <div>
                     <h2>Result❕</h2>
-                    <h2>{((result.correct+1)/questions.length)*100}%</h2>
-                    <h2>You have answered {result.correct+1} out of 5 questions correctly.</h2>
-                    <div className="re">
-                        <h2>Attempted:</h2>
-                        <h3>{result.Attempted+1}</h3>
-                    </div>
-                    <div className="re"> 
-                        <h2>correct</h2>
-                        <h3>{result.correct+1}</h3>
-                    </div>
-                    <div className="re">
-                        <h2>Wrong</h2>
-                        <h3>{result.wrong}</h3>
-                    </div>
+                    <h2>{((result==4&&curr==4?result+1:result)/questions.length)*100}%</h2>
+                    <h2>{result==4&&curr==4?result+1:result} out of 5 correct.</h2>
                     <Link to={"/"}><button>Replay😎</button></Link>
                 </div>
                 </dialog>
