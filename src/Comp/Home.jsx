8bebb99df1ab3f1be334902d1e61c64b27802168
Mohana Vamsi <mohanavamsi14@gmail.com>
2024-02-22 15:56:21 +0530

    Update Home.jsx

--- src/Comp/Home.jsx
+++ src/Comp/Home.jsx
@@ -3,8 +3,10 @@ import ein from "../assets/ein.jpg"
 function Home(){
     return(
     <>
+{/*         this for image */}
         <img src={ein} id="img"/>
         <h1>EinStino😛</h1>
+{/*         defined a route for go to quiz  */}
         <Link to="que"><h1 className="start">Start🤜</h1></Link>
     </>
     )
